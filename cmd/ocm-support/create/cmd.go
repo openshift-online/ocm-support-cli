d68410b6565fb98c15b399cd970804696dccb730
Tirth Chetan Thakkar <tirththakkar51@gmail.com>
2022-07-27 10:22:09 -0700

    Added capabilities implementation

--- cmd/ocm-support/create/cmd.go
+++ cmd/ocm-support/create/cmd.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"ocm-support-cli/cmd/ocm-support/utils"
 	"ocm-support-cli/pkg/account"
+	"ocm-support-cli/pkg/capability"
 	"ocm-support-cli/pkg/label"
 	"ocm-support-cli/pkg/organization"
 	"ocm-support-cli/pkg/subscription"
@@ -19,14 +20,15 @@ var args struct {
 	external bool
 }
 
-var vailidResources []string = []string{"accountLabel", "subscriptionLabel", "organizationLabel"}
+var vailidResources []string = []string{"accountLabel", "subscriptionLabel", "organizationLabel",
+	"accountCapability", "subscriptionCapability", "organizationCapability"}
 
 // Cmd ...
 var Cmd = &cobra.Command{
 	Use:   "create [accountLabel|subscriptionLabel|organizationLabel] [accountID|subscriptionID|organizationID] [key] [value]",
 	Short: "Creates the given resource with provided key and value.",
 	Long:  "Creates the given resource with provided key and value.",
-	Args:  cobra.MinimumNArgs(4),
+	Args:  cobra.MinimumNArgs(3),
 	RunE:  run,
 }
 
@@ -41,14 +43,10 @@ func init() {
 }
 
 func run(cmd *cobra.Command, argv []string) error {
-	if len(argv) != 4 {
-		return fmt.Errorf("expected exactly four arguments")
+	if len(argv) < 3 {
+		return fmt.Errorf("expected at least 3 arguments")
 	}
-	id := argv[1]
-	key := argv[2]
-	value := argv[3]
-
-	createdLabel, err := ManageOperations(argv[0], id, key, value, args.external)
+	createdLabel, err := ManageOperations(argv, args.external)
 	if err != nil {
 		return fmt.Errorf("%v", err)
 	}
@@ -56,42 +54,52 @@ func run(cmd *cobra.Command, argv []string) error {
 	return nil
 }
 
-func ManageOperations(action string, id string, key string, value string, external bool) (*v1.Label, error) {
+func ManageOperations(argv []string, external bool) (*v1.Label, error) {
+	action := argv[0]
+	if !slices.Contains(vailidResources, action) {
+		return nil, fmt.Errorf("invalid resource. Valid resources are: %v", vailidResources)
+	}
 	connection, err := ocm.NewConnection().Build()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create OCM connection: %v", err)
 	}
+	id := argv[1]
+	key := argv[2]
+	var value string
 	var createdLabel *v1.Label
-	if !slices.Contains(vailidResources, action) {
-		return nil, fmt.Errorf("invalid resource. Valid resources are: %v", vailidResources)
-	}
 	switch action {
 	case "accountLabel":
-		_, err = account.GetAccount(id, connection)
+		value = argv[3]
+		createdLabel, err = AddLabelToAccount(id, key, value, !args.external, connection)
 		if err != nil {
-			return nil, fmt.Errorf("failed to get account: %v", err)
-		}
-		createdLabel, err = account.AddLabel(id, key, value, !args.external, connection)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create label: %v", err)
+			return nil, fmt.Errorf("error creating label: %v", err)
 		}
 	case "subscriptionLabel":
-		_, err = subscription.GetSubscription(id, connection)
+		value = argv[3]
+		createdLabel, err = AddLabelToSubscription(id, key, value, !args.external, connection)
 		if err != nil {
-			return nil, fmt.Errorf("failed to get subscription: %v", err)
-		}
-		createdLabel, err = subscription.AddLabel(id, key, value, !args.external, connection)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create label: %v", err)
+			return nil, fmt.Errorf("error creating label: %v", err)
 		}
 	case "organizationLabel":
-		_, err = organization.GetOrganization(id, connection)
+		value = argv[3]
+		createdLabel, err = AddLabelToOrganization(id, key, value, !args.external, connection)
 		if err != nil {
-			return nil, fmt.Errorf("failed to get organization: %v", err)
+			return nil, fmt.Errorf("error creating label: %v", err)
 		}
-		createdLabel, err = organization.AddLabel(id, key, value, !args.external, connection)
+	case "accountCapability":
+		createdLabel, err = AddCapabilityToAccount(id, key, connection)
 		if err != nil {
-			return nil, fmt.Errorf("failed to create label: %v", err)
+			return nil, fmt.Errorf("failed to create capability: %v", err)
+		}
+	case "subscriptionCapability":
+		createdLabel, err = AddCapabilityToSubscription(id, key, connection)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create capability: %v", err)
+		}
+	case "organizationCapability":
+		createdLabel, err = AddCapabilityToOrganization(id, key, connection)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create capabilty: %v", err)
 		}
 	default:
 		return nil, fmt.Errorf("invalid argument")
@@ -133,4 +141,52 @@ func AddLabelToSubscription(subscriptionID string, key string, value string, isI
 		return nil, fmt.Errorf("failed to create label: %v", err)
 	}
 	return createdLabel, nil
+}
+
+func AddCapabilityToAccount(accountID string, key string, connection *sdk.Connection) (*v1.Label, error) {
+	_, err := account.GetAccount(accountID, connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get account: %v", err)
+	}
+	capabilityValue, err := capability.GetAvailableCapabilityValue(key, "account")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get capability: %v", err)
+	}
+	createdCapability, err := account.AddLabel(accountID, key, capabilityValue, true, connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create capability: %v", err)
+	}
+	return createdCapability, nil
+}
+
+func AddCapabilityToOrganization(accountID string, key string, connection *sdk.Connection) (*v1.Label, error) {
+	_, err := organization.GetOrganization(accountID, connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get organization: %v", err)
+	}
+	capabilityValue, err := capability.GetAvailableCapabilityValue(key, "organization")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get capability: %v", err)
+	}
+	createdCapability, err := organization.AddLabel(accountID, key, capabilityValue, true, connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create capability: %v", err)
+	}
+	return createdCapability, nil
+}
+
+func AddCapabilityToSubscription(accountID string, key string, connection *sdk.Connection) (*v1.Label, error) {
+	_, err := subscription.GetSubscription(accountID, connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get subscription: %v", err)
+	}
+	capabilityValue, err := capability.GetAvailableCapabilityValue(key, "cluster")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get capability: %v", err)
+	}
+	createdCapability, err := subscription.AddLabel(accountID, key, capabilityValue, true, connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create capability: %v", err)
+	}
+	return createdCapability, nil
 }
--- pkg/capability/capability.go
+++ pkg/capability/capability.go
@@ -1,6 +1,9 @@
 package capability
 
 import (
+	"fmt"
+	"strings"
+
 	v1 "github.com/openshift-online/ocm-sdk-go/accountsmgmt/v1"
 )
 
@@ -11,6 +14,69 @@ type Capability struct {
 }
 
 type CapabilityList []Capability
+
+// A list of known capabilities and notes on enabling them can be found on
+// https://docs.google.com/spreadsheets/d/1XMFkbG9DeXoBSOn5HPbzk-l2T_OjQdLjz452a6vaYCE
+
+// Capabilities must contain 3 sections, separated by "."
+// capability.{type}.{name}
+const CapabilityAggressiveClusterSetup = "capability.account.aggressive_cluster_cleanup"
+const CapabilityCreateMoaClusters = "capability.account.create_moa_clusters"
+const CapabilityManageClusterAdmin = "capability.cluster.manage_cluster_admin"
+const CapabilityOrganizationRegistrationsPerHour = "capability.organization.clusters_registrations_per_hour"
+const CapabilityOrganizationPinClusterToShard = "capability.organization.pin_cluster_to_shard"
+const CapabilityHibernateCluster = "capability.organization.hibernate_cluster"
+const CapabilitySubscribedOcp = "capability.cluster.subscribed_ocp"
+const CapabilitySubscribedOcpMarketplace = "capability.cluster.subscribed_ocp_marketplace"
+const CapabilitySubscribedOsdMarketplace = "capability.cluster.subscribed_osd_marketplace"
+const CapabilityEnableTermsEnforcement = "capability.account.enable_terms_enforcement"
+const CapabilityBareMetalInstallerAdmin = "capability.account.bare_metal_installer_admin"
+const CapabilityReleaseOcpClusters = "capability.cluster.release_ocp_clusters"
+const CapabilityAutoscaleClustersDeprecated = "capability.organization.autoscale_clusters"
+const CapabilityAutoscaleClusters = "capability.cluster.autoscale_clusters"
+const CapabilityOrganizationInstallConfigOverride = "capability.organization.install_config_override"
+const CapabilityOrganizationInstallConfigDefault = "capability.organization.install_config_default"
+const CapabilityOrganizationOverrideOsdTrialLength = "capability.organization.override_osdtrial_length_days"
+const CapabilityOrganizationCreateClusterProxy = "capability.organization.create_cluster_proxy"
+const CapabilityAllowGCPNonCCSPrivateClusters = "capability.organization.create_gcp_non_ccs_cluster"
+const CapabilityAllowInstallEOLVersions = "capability.organization.allow_install_eol_versions"
+const CapabilityAddOnVersionSelect = "capability.organization.addon_version_select"
+const CapabilityOrganizationFipsCluster = "capability.organization.fips_cluster"
+const CapabilityOrganizationOvnCluster = "capability.organization.ovn_cluster"
+const CapabilityOrganizationHyperShift = "capability.organization.hypershift"
+const CapabilityBypassMaxExpiration = "capability.organization.bypass_max_expiration"
+
+// This "use_rosa_paid_ami" capability will only be used short-term and will eventually be removed
+// See https://issues.redhat.com/browse/SDB-1698
+const CapabilityUseRosaPaidAMI = "capability.account.use_rosa_paid_ami"
+
+var availableCapabilities map[string]string = map[string]string{
+	"CapabilityAggressiveClusterSetup":             CapabilityAggressiveClusterSetup,
+	"CapabilityCreateMoaClusters":                  CapabilityCreateMoaClusters,
+	"CapabilityManageClusterAdmin":                 CapabilityManageClusterAdmin,
+	"CapabilityOrganizationRegistrationsPerHour":   CapabilityOrganizationRegistrationsPerHour,
+	"CapabilityOrganizationPinClusterToShard":      CapabilityOrganizationPinClusterToShard,
+	"CapabilityHibernateCluster":                   CapabilityHibernateCluster,
+	"CapabilitySubscribedOcp":                      CapabilitySubscribedOcp,
+	"CapabilitySubscribedOcpMarketplace":           CapabilitySubscribedOcpMarketplace,
+	"CapabilitySubscribedOsdMarketplace":           CapabilitySubscribedOsdMarketplace,
+	"CapabilityEnableTermsEnforcement":             CapabilityEnableTermsEnforcement,
+	"CapabilityBareMetalInstallerAdmin":            CapabilityBareMetalInstallerAdmin,
+	"CapabilityReleaseOcpClusters":                 CapabilityReleaseOcpClusters,
+	"CapabilityAutoscaleClustersDeprecated":        CapabilityAutoscaleClustersDeprecated,
+	"CapabilityAutoscaleClusters":                  CapabilityAutoscaleClusters,
+	"CapabilityOrganizationInstallConfigOverride":  CapabilityOrganizationInstallConfigOverride,
+	"CapabilityOrganizationInstallConfigDefault":   CapabilityOrganizationInstallConfigDefault,
+	"CapabilityOrganizationOverrideOsdTrialLength": CapabilityOrganizationOverrideOsdTrialLength,
+	"CapabilityOrganizationCreateClusterProxy":     CapabilityOrganizationCreateClusterProxy,
+	"CapabilityAllowGCPNonCCSPrivateClusters":      CapabilityAllowGCPNonCCSPrivateClusters,
+	"CapabilityAddOnVersionSelect":                 CapabilityAddOnVersionSelect,
+	"CapabilityOrganizationFipsCluster":            CapabilityOrganizationFipsCluster,
+	"CapabilityOrganizationOvnCluster":             CapabilityOrganizationOvnCluster,
+	"CapabilityOrganizationHyperShift":             CapabilityOrganizationHyperShift,
+	"CapabilityBypassMaxExpiration":                CapabilityBypassMaxExpiration,
+	"CapabilityUseRosaPaidAMI":                     CapabilityUseRosaPaidAMI,
+}
 
 func PresentCapabilities(capabilities []*v1.Capability) CapabilityList {
 	var capabilitiesList []Capability
@@ -23,4 +89,15 @@ func PresentCapabilities(capabilities []*v1.Capability) CapabilityList {
 		capabilitiesList = append(capabilitiesList, cap)
 	}
 	return capabilitiesList
+}
+
+func GetAvailableCapabilityValue(capability string, resourceType string) (string, error) {
+	val, ok := availableCapabilities[capability]
+	if !ok {
+		return "", fmt.Errorf("capability not found")
+	}
+	if strings.Split(val, ".")[1] != resourceType {
+		return "", fmt.Errorf("capability not available for given resource type")
+	}
+	return val, nil
 }
