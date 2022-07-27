b7673e457d66566f00555c38b2a034066f2b9e8e
Tirth Chetan Thakkar <tirththakkar51@gmail.com>
2022-07-27 07:13:51 -0700

    Fixed review comments

--- cmd/ocm-support/create/cmd.go
+++ cmd/ocm-support/create/cmd.go
@@ -9,6 +9,7 @@ import (
 	"ocm-support-cli/pkg/subscription"
 
 	"github.com/openshift-online/ocm-cli/pkg/ocm"
+	sdk "github.com/openshift-online/ocm-sdk-go"
 	v1 "github.com/openshift-online/ocm-sdk-go/accountsmgmt/v1"
 	"github.com/spf13/cobra"
 )
@@ -17,11 +18,7 @@ var args struct {
 	external bool
 }
 
-const (
-	accountLabel      = "accountLabel"
-	subscriptionLabel = "subscriptionLabel"
-	organizationLabel = "organizationLabel"
-)
+var vailidResources []string = []string{"accountLabel", "subscriptionLabel", "organizationLabel"}
 
 // Cmd ...
 var Cmd = &cobra.Command{
@@ -46,43 +43,90 @@ func run(cmd *cobra.Command, argv []string) error {
 	if len(argv) != 4 {
 		return fmt.Errorf("expected exactly four arguments")
 	}
+	id := argv[1]
+	key := argv[2]
+	value := argv[3]
 
-	connection, err := ocm.NewConnection().Build()
+	createdLabel, err := ManageOperations(argv[0], id, key, value, args.external)
 	if err != nil {
-		return fmt.Errorf("failed to create OCM connection: %v", err)
-	}
-	var createdLabel *v1.Label
-	switch argv[0] {
-	case accountLabel:
-		_, err := account.GetAccount(argv[1], connection)
-		if err != nil {
-			return fmt.Errorf("failed to get account: %v", err)
-		}
-		createdLabel, err = account.AddLabel(argv[1], argv[2], argv[3], !args.external, connection)
-		if err != nil {
-			return fmt.Errorf("failed to create label: %v", err)
-		}
-	case subscriptionLabel:
-		_, err := subscription.GetSubscription(argv[1], connection)
-		if err != nil {
-			return fmt.Errorf("failed to get subscription: %v", err)
-		}
-		createdLabel, err = subscription.AddLabel(argv[1], argv[2], argv[3], !args.external, connection)
-		if err != nil {
-			return fmt.Errorf("failed to create label: %v", err)
-		}
-	case organizationLabel:
-		_, err := organization.GetOrganization(argv[1], connection)
-		if err != nil {
-			return fmt.Errorf("failed to get organization: %v", err)
-		}
-		createdLabel, err = organization.AddLabel(argv[1], argv[2], argv[3], !args.external, connection)
-		if err != nil {
-			return fmt.Errorf("failed to create label: %v", err)
-		}
-	default:
-		return fmt.Errorf("invalid argument")
+		return fmt.Errorf("%v", err)
 	}
 	utils.PrettyPrint(label.PresentLabels([]*v1.Label{createdLabel}))
 	return nil
+}
+
+func ManageOperations(action string, id string, key string, value string, external bool) (*v1.Label, error) {
+	connection, err := ocm.NewConnection().Build()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create OCM connection: %v", err)
+	}
+	var createdLabel *v1.Label
+	switch action {
+	case "accountLabel":
+		_, err = account.GetAccount(id, connection)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get account: %v", err)
+		}
+		createdLabel, err = account.AddLabel(id, key, value, !args.external, connection)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create label: %v", err)
+		}
+	case "subscriptionLabel":
+		_, err = subscription.GetSubscription(id, connection)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get subscription: %v", err)
+		}
+		createdLabel, err = subscription.AddLabel(id, key, value, !args.external, connection)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create label: %v", err)
+		}
+	case "organizationLabel":
+		_, err = organization.GetOrganization(id, connection)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get organization: %v", err)
+		}
+		createdLabel, err = organization.AddLabel(id, key, value, !args.external, connection)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create label: %v", err)
+		}
+	default:
+		return nil, fmt.Errorf("invalid argument")
+	}
+	return createdLabel, nil
+}
+
+func AddLabelToAccount(accountID string, key string, value string, isInternal bool, connection *sdk.Connection) (*v1.Label, error) {
+	_, err := account.GetAccount(accountID, connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get account: %v", err)
+	}
+	createdLabel, err := account.AddLabel(accountID, key, value, !args.external, connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create label: %v", err)
+	}
+	return createdLabel, nil
+}
+
+func AddLabelToOrganization(orgID string, key string, value string, isInternal bool, connection *sdk.Connection) (*v1.Label, error) {
+	_, err := organization.GetOrganization(orgID, connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get organization: %v", err)
+	}
+	createdLabel, err := organization.AddLabel(orgID, key, value, !args.external, connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create label: %v", err)
+	}
+	return createdLabel, nil
+}
+
+func AddLabelToSubscription(subscriptionID string, key string, value string, isInternal bool, connection *sdk.Connection) (*v1.Label, error) {
+	_, err := subscription.GetSubscription(subscriptionID, connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get subscription: %v", err)
+	}
+	createdLabel, err := subscription.AddLabel(subscriptionID, key, value, !args.external, connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create label: %v", err)
+	}
+	return createdLabel, nil
 }
